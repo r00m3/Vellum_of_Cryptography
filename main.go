f7d68a371c425a82d6a4984ad536c62029a8e14b
r00m3 <169680238+r00m3@users.noreply.github.com>
2024-05-17 18:18:18 +0300

    Update main.go

--- main.go
+++ main.go
@@ -13,46 +13,35 @@ func clearTerminal() {
 	fmt.Printf("\x1b[3J")
 }
 
-// func scrollUp() {
-// 	fmt.Printf(" ")
-// }
-
 func scanForPage() {
 	fmt.Scanf("%s", &page)
 	if page == "0" {
 		clearTerminal()
 		text.PrintREADME()
 		scanForPage()
-		// scrollUp()
 	} else if page == "1" {
 		clearTerminal()
 		text.PrintHashFunctions()
 		scanForPage()
-		// scrollUp()
 	} else if page == "2" {
 		clearTerminal()
 		text.PrintAuthenticatedEncryption()
 		scanForPage()
-		// scrollUp()
 	} else if page == "3" {
 		clearTerminal()
 		text.PrintSymmetricEncryption()
 		scanForPage()
-		// scrollUp()
 	} else if page == "4" {
 		clearTerminal()
 		text.PrintAsymmetricEncryption()
 		scanForPage()
-		// scrollUp()
 	} else {
 		scanForPage()
 	}
-
 }
 
 func main() {
 	clearTerminal()
 	text.PrintREADME()
-	// scrollUp()
 	scanForPage()
 }
